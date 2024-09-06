e5b60928532e9899e1cdc79d3b9855bcf8ad3650
Logan Parke <logan.c.parke@gmail.com>
2024-09-06 13:25:22 -0600

    updated styles

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,14 +11,13 @@ export default function About(){
     <>
       <Navbar/>
       <section>
-      <Image alt='pic' src={pic1} className='w-[100vw] mt-[-150px]'/>
       <h1>David Pistol</h1>
       </section>
       
 
-      <section>
-        <Image  alt='pic' src={pic1}/>
-        <div>
+      <section className='flex justify-center items-center'>
+        <Image className='w-[80%] max-w-[500px] mr-24' alt='pic' src={pic1}/>
+        <div className='w-[45%]'>
           <h5>The Woman Behind The Lens.</h5>
           <p>inspired by artists like Gord Parker and Yousuf Karsh, uses light and storytelling to capture the transformative power of art in our city. Through meticulous attention to detail and self-portraiture, his images evoke emotions and inspire change, celebrating community and fostering self-reflection. Each photo is a gift, illuminating spaces with hope and diversity, inviting viewers to see the world through a lens of compassion and love.</p>
           <a href="/portfolio"> view portfolio</a>

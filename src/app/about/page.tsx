0732b25a911a9888455c99ffc508b86d149b4818
Logan Parke <logan.c.parke@gmail.com>
2024-09-06 13:25:27 -0600

    about page work

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,6 @@
-import pic1 from '../../../public/images/pic2.jpg'
-
+import pic1 from '../../../public/images/pic2.jpg';
+import about1 from '../../../public/images/about1.jpg'
+import styles from './styles.module.css';
 import Image from 'next/image';
 
 import Navbar from '@/components/NavBar';
@@ -10,17 +11,23 @@ export default function About(){
   return (
     <>
       <Navbar/>
-      <section>
-      <h1>David Pistol</h1>
+      <div className={styles.aboutWrap}>
+      <section className={styles.aboutHero}>
+      <h1 className='text-7xl  drop-shadow-lg'>David Pistol</h1>
       </section>
       
 
-      <section className='flex justify-center items-center'>
-        <Image className='w-[80%] max-w-[500px] mr-24' alt='pic' src={pic1}/>
+      <section className='flex justify-center items-center p-36'>
+        <div className='w-[80%] max-w-[500px] mr-24' >
+        <Image className='w-[150%]' alt='pic' src={about1}/>
+        </div>
+        
         <div className='w-[45%]'>
-          <h5>The Woman Behind The Lens.</h5>
-          <p>inspired by artists like Gord Parker and Yousuf Karsh, uses light and storytelling to capture the transformative power of art in our city. Through meticulous attention to detail and self-portraiture, his images evoke emotions and inspire change, celebrating community and fostering self-reflection. Each photo is a gift, illuminating spaces with hope and diversity, inviting viewers to see the world through a lens of compassion and love.</p>
-          <a href="/portfolio"> view portfolio</a>
+          <h5 className='relative bottom-12 text-3xl'>The Woman Behind The Lens.</h5>
+          <div className='ml-auto max-w-[500px]'>
+          <p className='flex  max-w-[400px] mr-20 mb-12'>inspired by artists like Gord Parker and Yousuf Karsh, uses light and storytelling to capture the transformative power of art in our city. Through meticulous attention to detail and self-portraiture, his images evoke emotions and inspire change, celebrating community and fostering self-reflection. Each photo is a gift, illuminating spaces with hope and diversity, inviting viewers to see the world through a lens of compassion and love.</p>
+          <a href="/portfolio" className='relative top-20 w-fit font-bold bg-white text-black p-5 pl-8 pr-8 rounded-[100px]'> VIEW PORTFOLIO</a>
+          </div>
         </div>
       </section>
       <section>
@@ -53,6 +60,7 @@ export default function About(){
           <a href='/contact'>LET'S CREATE</a>
         </div>
       </section>
+    </div>
     </>
   )
 }

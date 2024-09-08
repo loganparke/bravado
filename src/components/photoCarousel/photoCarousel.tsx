cd8681a509f5440ed9b1c2c2c233ed2533685fce
Logan Parke <logan.c.parke@gmail.com>
2024-09-08 10:39:16 -0600

    photo carousel

--- src/components/photoCarousel/photoCarousel.tsx
+++ src/components/photoCarousel/photoCarousel.tsx
@@ -1,6 +1,9 @@
 import pic from '../../../public/images/about1.jpg';
 import styles from './photoCarousel.module.css';
 
+import { HiOutlineArrowLongLeft } from "react-icons/hi2";
+import { HiOutlineArrowLongRight } from "react-icons/hi2";
+
 import Image from 'next/image';
 
 const PhotoCarosuel: React.FC = () => {
@@ -14,6 +17,15 @@ const PhotoCarosuel: React.FC = () => {
         <Image alt='image' src={pic} width={400}/>
         <Image alt='image' src={pic} width={400}/>
       </section>
+      <div className='flex w-fit space-between m-auto'>
+      <HiOutlineArrowLongLeft className='flex my-auto text-4xl cursor-pointer'/>
+        <p className='text-center mx-4 text-xl'>
+          01
+          <br/>
+          / 06  
+        </p>
+        <HiOutlineArrowLongRight className='flex my-auto text-4xl cursor-pointer'/>
+      </div>
     </div>
   );
 };

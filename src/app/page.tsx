813fe5ffe042325d869afe9f0b7033cc3f65d3a9
Logan Parke <logan.c.parke@gmail.com>
2024-09-09 13:26:07 -0600

    finished photo animation on the homescreen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,12 +39,98 @@ export default function Home(){
     
     let bg = target.children[1].children[0];
     let text = target.children[1].children[1];
-    console.log(bg, text);
-    console.log(`Mouse ${action} from the ${side}`);
+
+    if(action == 'entered'){
     if(side == 'left'){
+      bg.classList.remove('transition-from-bottom')
+      bg.classList.remove('transition-from-left')
+      bg.classList.remove('transition-from-right')
+      bg.classList.remove('transition-from-top')
+      bg.classList.add('transition-from-left')
+      bg.style.maxHeight = '800px';
+      bg.style.maxWidth = '400px';
+      text.style.opacity = '1';
+    } else if (side == 'top'){
+      bg.classList.remove('transition-from-bottom')
+      bg.classList.remove('transition-from-left')
+      bg.classList.remove('transition-from-right')
+      bg.classList.remove('transition-from-top')
+      bg.classList.add('transition-from-top')
+      bg.style.maxHeight = '800px';
+      bg.style.maxWidth = '400px';
+      text.style.opacity = '1';
+    } else if (side == 'right'){
+      bg.classList.remove('transition-from-bottom')
+      bg.classList.remove('transition-from-left')
+      bg.classList.remove('transition-from-right')
+      bg.classList.remove('transition-from-top')
+      bg.classList.add('transition-from-right')
+      
+      bg.style.maxHeight = '800px';
+      bg.style.maxWidth = '400px';
+      text.style.opacity = '1';
+    } else if (side == 'bottom'){
+      bg.classList.remove('transition-from-bottom')
+      bg.classList.remove('transition-from-left')
+      bg.classList.remove('transition-from-right')
+      bg.classList.remove('transition-from-top')
+      bg.classList.add('transition-from-bottom')
+      bg.style.maxHeight = '800px';
       bg.style.maxWidth = '400px';
       text.style.opacity = '1';
     }
+  }
+
+  if(action == "left"){
+    if(side == 'left'){
+      bg.classList.remove('transition-from-bottom')
+      bg.classList.remove('transition-from-left')
+      bg.classList.remove('transition-from-right')
+      bg.classList.remove('transition-from-top')
+      bg.classList.add('transition-from-left')
+      bg.style.maxWidth = '0px';
+      text.style.opacity = '0';
+      setTimeout(() => {
+        bg.style.maxHeight = '0px';
+      }, 300);
+
+    } else if (side == 'right'){
+      bg.classList.remove('transition-from-bottom')
+      bg.classList.remove('transition-from-left')
+      bg.classList.remove('transition-from-right')
+      bg.classList.remove('transition-from-top')
+      bg.classList.add('transition-from-right')
+      bg.style.maxWidth = '0px';
+      text.style.opacity = '0';
+      setTimeout(() => {
+        bg.style.maxHeight = '0px';
+      }, 300);
+
+    } else if (side == 'top'){
+      bg.classList.remove('transition-from-bottom')
+      bg.classList.remove('transition-from-left')
+      bg.classList.remove('transition-from-right')
+      bg.classList.remove('transition-from-top')
+      bg.classList.add('transition-from-top')
+      bg.style.maxHeight = '0px';
+      text.style.opacity = '0';
+      setTimeout(() => {
+        bg.style.maxWidth = '0px';
+      }, 300);
+      
+    } else if (side == 'bottom'){
+      bg.classList.remove('transition-from-bottom')
+      bg.classList.remove('transition-from-left')
+      bg.classList.remove('transition-from-right')
+      bg.classList.remove('transition-from-top')
+      bg.classList.add('transition-from-bottom')
+      bg.style.maxHeight = '0px';
+      text.style.opacity = '0';
+      setTimeout(() => {
+        bg.style.maxWidth = '0px';
+      }, 300);
+    }
+  }
   };
   const handleMouseEnter = (event: MouseEvent<HTMLDivElement>) => {
     determineSide(event, 'entered');
@@ -63,19 +149,67 @@ export default function Home(){
       <section className='w-[1200px] max-w-[100%] flex m-auto justify-center flex-wrap mb-20 '>
         <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}  className='max-w-[90%] w-[330px] mx-4 my-4 cursor-pointer relative'>
         <Image alt='pic' src={pic}/>
-        <article className='absolute w-full h-full top-0 left-0 right-0 bottom-0 flex align-center justify-center'>
-          <div className={`custom-transition bg-gray-800/[.8] absolute w-full h-full top-0 left-0 right-0 bottom-0 max-w-[0px]`}></div>
+        <article className='absolute w-full h-full top-0 bottom-0 flex align-center justify-center'>
+          <div className={` bg-gray-800/[.8] absolute w-full h-full  max-w-[0px] max-h-[0px]`}></div>
           <p className='custom-transition flex relative w-fit h-fit text-center mx-auto my-auto opacity-0'>DOUBLE VISION</p>
         </article>
         </div>
-        <Image alt='pic' src={pic} className='max-w-[90%] w-[330px] mx-4 my-4 cursor-pointer'/>
-        <Image alt='pic' src={pic} className='max-w-[90%] w-[330px] mx-4 my-4 cursor-pointer'/>
-        <Image alt='pic' src={pic} className='max-w-[90%] w-[330px] mx-4 my-4 cursor-pointer'/>
-        <Image alt='pic' src={pic} className='max-w-[90%] w-[330px] mx-4 my-4 cursor-pointer'/>
-        <Image alt='pic' src={pic} className='max-w-[90%] w-[330px] mx-4 my-4 cursor-pointer'/>
-        <Image alt='pic' src={pic} className='max-w-[90%] w-[330px] mx-4 my-4 cursor-pointer'/>
-        <Image alt='pic' src={pic} className='max-w-[90%] w-[330px] mx-4 my-4 cursor-pointer'/>
-        <Image alt='pic' src={pic} className='max-w-[90%] w-[330px] mx-4 my-4 cursor-pointer'/>
+        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}  className='max-w-[90%] w-[330px] mx-4 my-4 cursor-pointer relative'>
+        <Image alt='pic' src={pic}/>
+        <article className='absolute w-full h-full top-0 bottom-0 flex align-center justify-center'>
+          <div className={` bg-gray-800/[.8] absolute w-full h-full  max-w-[0px] max-h-[0px]`}></div>
+          <p className='custom-transition flex relative w-fit h-fit text-center mx-auto my-auto opacity-0'>DOUBLE VISION</p>
+        </article>
+        </div>
+        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}  className='max-w-[90%] w-[330px] mx-4 my-4 cursor-pointer relative'>
+        <Image alt='pic' src={pic}/>
+        <article className='absolute w-full h-full top-0 bottom-0 flex align-center justify-center'>
+          <div className={` bg-gray-800/[.8] absolute w-full h-full  max-w-[0px] max-h-[0px]`}></div>
+          <p className='custom-transition flex relative w-fit h-fit text-center mx-auto my-auto opacity-0'>DOUBLE VISION</p>
+        </article>
+        </div>
+        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}  className='max-w-[90%] w-[330px] mx-4 my-4 cursor-pointer relative'>
+        <Image alt='pic' src={pic}/>
+        <article className='absolute w-full h-full top-0 bottom-0 flex align-center justify-center'>
+          <div className={` bg-gray-800/[.8] absolute w-full h-full  max-w-[0px] max-h-[0px]`}></div>
+          <p className='custom-transition flex relative w-fit h-fit text-center mx-auto my-auto opacity-0'>DOUBLE VISION</p>
+        </article>
+        </div>
+        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}  className='max-w-[90%] w-[330px] mx-4 my-4 cursor-pointer relative'>
+        <Image alt='pic' src={pic}/>
+        <article className='absolute w-full h-full top-0 bottom-0 flex align-center justify-center'>
+          <div className={` bg-gray-800/[.8] absolute w-full h-full  max-w-[0px] max-h-[0px]`}></div>
+          <p className='custom-transition flex relative w-fit h-fit text-center mx-auto my-auto opacity-0'>DOUBLE VISION</p>
+        </article>
+        </div>
+        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}  className='max-w-[90%] w-[330px] mx-4 my-4 cursor-pointer relative'>
+        <Image alt='pic' src={pic}/>
+        <article className='absolute w-full h-full top-0 bottom-0 flex align-center justify-center'>
+          <div className={` bg-gray-800/[.8] absolute w-full h-full  max-w-[0px] max-h-[0px]`}></div>
+          <p className='custom-transition flex relative w-fit h-fit text-center mx-auto my-auto opacity-0'>DOUBLE VISION</p>
+        </article>
+        </div>
+        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}  className='max-w-[90%] w-[330px] mx-4 my-4 cursor-pointer relative'>
+        <Image alt='pic' src={pic}/>
+        <article className='absolute w-full h-full top-0 bottom-0 flex align-center justify-center'>
+          <div className={` bg-gray-800/[.8] absolute w-full h-full  max-w-[0px] max-h-[0px]`}></div>
+          <p className='custom-transition flex relative w-fit h-fit text-center mx-auto my-auto opacity-0'>DOUBLE VISION</p>
+        </article>
+        </div>
+        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}  className='max-w-[90%] w-[330px] mx-4 my-4 cursor-pointer relative'>
+        <Image alt='pic' src={pic}/>
+        <article className='absolute w-full h-full top-0 bottom-0 flex align-center justify-center'>
+          <div className={` bg-gray-800/[.8] absolute w-full h-full  max-w-[0px] max-h-[0px]`}></div>
+          <p className='custom-transition flex relative w-fit h-fit text-center mx-auto my-auto opacity-0'>DOUBLE VISION</p>
+        </article>
+        </div>
+        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}  className='max-w-[90%] w-[330px] mx-4 my-4 cursor-pointer relative'>
+        <Image alt='pic' src={pic}/>
+        <article className='absolute w-full h-full top-0 bottom-0 flex align-center justify-center'>
+          <div className={` bg-gray-800/[.8] absolute w-full h-full  max-w-[0px] max-h-[0px]`}></div>
+          <p className='custom-transition flex relative w-fit h-fit text-center mx-auto my-auto opacity-0'>DOUBLE VISION</p>
+        </article>
+        </div>
       </section>
     </>
   )

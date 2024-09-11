1e945cbfa8977dbc0e708dcff8eec7824cf44530
Logan Parke <logan.c.parke@gmail.com>
2024-09-11 11:11:49 -0600

    block nav homescreen

--- src/components/blockNavigation/blockNav.tsx
+++ src/components/blockNavigation/blockNav.tsx
@@ -3,44 +3,59 @@ import { useRef } from 'react';
 
 const BlockNav: React.FC = () => {
 
-  let portfolio = useRef<HTMLAnchorElement>(null);
-  let blog = useRef<HTMLAnchorElement>(null);
-  let about = useRef<HTMLAnchorElement>(null);
+  let aboutSect = useRef<HTMLDivElement | null>(null);
 
-  const handleMouseEnter = (event: MouseEvent<HTMLDivElement>) => {
+  let portfolio = useRef<HTMLAnchorElement | null>(null);
+  let blog = useRef<HTMLAnchorElement | null>(null);
+  let about = useRef<HTMLAnchorElement | null>(null);
+
+  const handleMouseEnter = (event: React.MouseEvent<HTMLAnchorElement>) => {
+
+    const target = event.currentTarget as HTMLAnchorElement;
     
-    let tab = event.target.children[0].children[1].textContent;
+    let tab = target.children[0].children[1].textContent;
   if(tab == 'PORTFOLIO'){
-    console.log('portfolio')
+    blog.current?.classList.remove(styles.highlighted)
+    portfolio.current?.classList.add(styles.highlighted)
+    about.current?.classList.remove(styles.highlighted)
+    aboutSect.current?.classList.remove(styles.bg2)
+    aboutSect.current?.classList.remove(styles.bg3)
+    aboutSect.current?.classList.add(styles.bg1)
   } else if(tab == 'BLOG'){
-    console.log(blog.current)
-    blog.current.classList.add('highlighted')
-    portfolio.classList.remove('higlighted')
-    
+    blog.current?.classList.add(styles.highlighted)
+    portfolio.current?.classList.remove(styles.highlighted)
+    about.current?.classList.remove(styles.highlighted)
+    aboutSect.current?.classList.remove(styles.bg1)
+    aboutSect.current?.classList.remove(styles.bg3)
+    aboutSect.current?.classList.add(styles.bg2)
   }else if(tab == 'ABOUT ME'){
-    console.log('ABOUT ME')
+    blog.current?.classList.remove(styles.highlighted)
+    portfolio.current?.classList.remove(styles.highlighted)
+    about.current?.classList.add(styles.highlighted)
+    aboutSect.current?.classList.remove(styles.bg1)
+    aboutSect.current?.classList.remove(styles.bg2)
+    aboutSect.current?.classList.add(styles.bg3)
   }
 
-    console.log(tab)
   };
 
   return (
-    <section className={styles.about}>
+    <section ref={aboutSect} className={styles.about}>
       <section>
       <div >
-      <a href="/portfolio" className={styles.highlighted}  onMouseEnter={handleMouseEnter}  ref={portfolio}>
+      <a href="/portfolio" className={`${styles.highlighted}`}  onMouseEnter={handleMouseEnter}  ref={portfolio}>
         <article>
           <p>view my work</p>
           <h2>PORTFOLIO</h2>
         </article>
       </a>
-      <a href="/blog" onMouseEnter={handleMouseEnter} ref={blog}>
+      <a href="/blog" className='h' onMouseEnter={handleMouseEnter} ref={blog}>
         <article>
           <p>let my thoughts stimulate your mind</p>
           <h2>BLOG</h2>
         </article>
       </a>
-      <a href="/about" onMouseEnter={handleMouseEnter} ref={about}>
+      <a href="/about" className='h' onMouseEnter={handleMouseEnter} ref={about}>
         <article>
           <p>get to know me</p>
           <h2>ABOUT ME</h2>
